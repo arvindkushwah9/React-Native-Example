feb031fb92b6c4a7c01adb79987b16db15adcb6d
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-13 14:23:24 +0530

    Added Flash Screen

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import Routes from './src/Router/Routes.js'
+import SplashScreen from 'react-native-splash-screen';
 
 class App extends Component {
+
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+
+
    render() {
       return (
          <Routes />
